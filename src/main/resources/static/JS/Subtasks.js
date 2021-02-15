d7743cec4814c8e636f5de6d35eda9e25cbe479f
calfonsoQA <75623928+calfonsoQA@users.noreply.github.com>
2021-02-15 12:40:04 +0000

    dropdown list for subtask create working

--- src/main/resources/static/JS/Subtasks.js
+++ src/main/resources/static/JS/Subtasks.js
@@ -15,6 +15,7 @@ const testDeleteButton = document.querySelector("#testDelete");
 //---------- FOR CREATE FORM------------------------------------
 const subtaskDesc = document.querySelector("#subtaskDescription");
 const eLevel = document.querySelector("#eLevel");
+const taskSelector = document.querySelector("#taskDropdown");
 const alert = document.querySelector("#onsuccess");
 //--------------------------------------------------------------
 //---------- FOR UPDATE FORM------------------------------------
@@ -36,19 +37,27 @@ const printTaskToScreen = (taskTitle) => {
     taskrowinside.appendChild(taskrow);
 }
 
+const taskToDropDown = (taskName, id) =>{
+    let taskOption = document.createElement("option");
+    taskOption.value=id;
+    let tasktext = document.createTextNode(`${taskName}`);
+    taskOption.appendChild(tasktext);
+    taskSelector.appendChild(taskOption);
+}
+
 const printSubtaskToScreen = (done, stasks, effort, taskId, i, taskName) => {
     let subtaskrow = document.createElement("div");
     subtaskrow.className = "row justify-content-around row-wireframe";
     let taskrowinside = document.createElement("div");
     taskrowinside.className = "alert alert-primary";
 
-    if (i == 0){
-    let taskrow = document.createElement("div");
-    let taskHead = document.createElement("h5");
-    let tasktext = document.createTextNode(`${taskName}`);
-    taskHead.appendChild(tasktext);
-    taskrow.appendChild(taskHead);
-    taskListOutput.appendChild(taskrow);
+    if (i == 0) {
+        let taskrow = document.createElement("div");
+        let taskHead = document.createElement("h5");
+        let tasktext = document.createTextNode(`${taskName}`);
+        taskHead.appendChild(tasktext);
+        taskrow.appendChild(taskHead);
+        taskListOutput.appendChild(taskrow);
     }
 
     let editColumn = document.createElement("div");
@@ -153,13 +162,12 @@ const getSubtask = () => {
                 console.log(`response is OK (200)`);
                 response.json().then((infofromserver) => {
                     console.log(infofromserver);
-                    console.log(infofromserver); 
                     for (let tasks of infofromserver) {
                         let i = 0;
                         for (let subtasks of tasks.subtaskList) {
                             console.log(i);
                             console.log(subtasks.subtaskDescription);
-                            printSubtaskToScreen(subtasks.done, subtasks.subtaskDescription, subtasks.effortLevel, subtasks.id, i ,tasks.taskName);
+                            printSubtaskToScreen(subtasks.done, subtasks.subtaskDescription, subtasks.effortLevel, subtasks.id, i, tasks.taskName);
                             i++;
                         }
                     }
@@ -170,13 +178,38 @@ const getSubtask = () => {
         })
 }
 
+const getTask = () => {
+    fetch("http://localhost:8080/task/readAll")
+        .then((response) => {
+
+            if (response.status !== 200) {
+                throw new Error("I don't have a status of 200");
+            } else {
+                console.log(response);
+                console.log(`response is OK (200)`);
+                response.json().then((infofromserver) => {
+                    console.log(infofromserver);
+                    console.log(infofromserver);
+                    for (let tasks of infofromserver) {
+                        taskToDropDown(tasks.taskName, tasks.id);
+                    }
+                })
+            }
+        }).catch((err) => {
+            console.error(err);
+        })
+}
 const createSubtask = () => {
     const subtaskDescriptionValue = subtaskDesc.value;
     const effortLevelValue = eLevel.value;
+    const taskSelectorValue = taskSelector.value;
+    console.log(taskSelectorValue);
 
     let data = {
         subtaskDescription: subtaskDescriptionValue,
-        effortLevel: effortLevelValue
+        effortLevel: effortLevelValue,
+        done:false,
+        myTask:{id:taskSelectorValue}
     }
 
     fetch("http://localhost:8080/subtask/create", {
@@ -200,6 +233,36 @@ const createSubtask = () => {
         })
         .catch(err => console.error(`Stopppppp! ${err}`));
 }
+// const createSubtask = () => {
+//     const subtaskDescriptionValue = subtaskDesc.value;
+//     const effortLevelValue = eLevel.value;
+
+//     let data = {
+//         subtaskDescription: subtaskDescriptionValue,
+//         effortLevel: effortLevelValue
+//     }
+
+//     fetch("http://localhost:8080/subtask/create", {
+//         method: "POST",
+//         body: JSON.stringify(data),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(info => {
+//             console.log(info);
+//             alert.setAttribute("class", "alert alert-success");
+//             alert.innerHTML = "Subtask has been successfully created!";
+//             //    getSubtask(); 
+//             setTimeout(() => {
+//                 alert.removeAttribute("class");
+//                 alert.innerHTML = "";
+
+//             }, 2000);
+//         })
+//         .catch(err => console.error(`Stopppppp! ${err}`));
+// }
 
 const updateSubtask = (id) => {
     const subtaskDescriptionValue = subtaskDescPUT.value;
@@ -257,5 +320,7 @@ const deleteSubtask = (id) => {
 
 
 getSubtask();
+getTask();
+
 // getTaskID();
 
